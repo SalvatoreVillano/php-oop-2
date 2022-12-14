59d7bc346e1e76bf0eb5389ae9775f6ab0604f72
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-12-15 00:57:22 +0100

    added other products

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ require_once __DIR__ . './Models/Category.php';
 
 
 $food1 = new Food('Bocconcini con pollo', 3.50, 8, new Category("Gatto"), "https://www.spesasprint.it/img/prodotti/big/57165.jpg?v=2", 4.25, "Pollo");
+$food2 = new Food('Crocchette di manzo', 18, 4, new Category("Cane"), "https://www.damoreno.ch/media/catalog/product/cache/5b5d7988e9af9f38308c6dd6deb78357/3/0/3010470154476_1ntfebazhkbzboao.jpg", 18, "Manzo");
+$toy1 = new Toys('Pallina rimbalzante', 5.20, 11, new Category("Cane"), "https://ae01.alicdn.com/kf/H29af1bcda7c148a3ae3b88cf5505ccc8h/Accendi-palline-per-cani-lampeggiante-palla-elastica-LED-palla-molare-palla-incandescente-Pet-colore-luce-palla.jpg_Q90.jpg_.webp", "Plastica");
+$toy2 = new Toys('Tiragraffi', 22.50, 2, new Category("Gatto"), "https://www.agrizoo2.it/wp-content/uploads/2022/05/48534_2.png", "Sisal");
+$kennel1 = new Kennel('Cuccia avvolgente', 13.80, 6, new Category('Gatto'), 'https://m.media-amazon.com/images/I/51ARMnCOFmL._SL500_.jpg', 'Grigio Chiaro');
+$kennel2 = new Kennel('Cuccia luxury', 29, 1, new Category('Cane'), 'https://www.original-legno.com/wp-content/uploads/2021/08/Cuccia_in_legno_Oasi_con_veranda_per_cani_barboncino.webp', 'Marrone');
+$product = [];
+array_push($product, $food1, $food2, $toy1, $toy2, $kennel1, $kennel2);
+
 // var_dump($food1);
 ?>
 
@@ -14,32 +22,34 @@ $food1 = new Food('Bocconcini con pollo', 3.50, 8, new Category("Gatto"), "https
 
 <div class="container">
     <div class="row">
-        <div class="col-3">
-            <div class="card">
-                <img src="<?php echo $food1->img ?>" class="card-img-top" alt="">
+    <?php foreach ($product as $obj) { ?>
+        <div class="col-4 my-4">
+            <div class="card h-100">
+                <img src="<?php echo $obj->img ?>" class="card-img-top" alt="">
                 <div class="card-body">
                     <p class="card-title fs-4 fw-bold">
-                        <?php echo $food1->name ?>
+                        <?php echo $obj->name ?>
 </p>
                     <p class="card-text">
                         Prezzo:
-                        <?php echo $food1->price ?>€
+                        <?php echo $obj->price ?>€
                     </p>
                     <p class="card-text">
                         Quantità:
-                        <?php echo $food1->quantity ?> rimanenti
+                        <?php echo $obj->quantity ?> rimanenti
                     </p>
                     <p class="card-text">
                         Tipo:
-                        <?php echo get_class($food1) ?>
+                        <?php echo get_class($obj) ?>
                     </p>
                     <p class="card-text">
                         Categoria:
-                        <?php echo $food1->category->type; ?>
+                        <?php echo $obj->category->type; ?>
                     </p>
                 </div>
             </div>
         </div>
+        <?php } ?>
     </div>
 </div>
 </body>
